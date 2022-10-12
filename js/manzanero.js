7d827e374585560fe317f1d5092c4ac8bdf7fa2e
COSNERMASTER <115478651+COSNERMASTER@users.noreply.github.com>
2022-10-12 14:11:52 -0400

    Add files via upload

---
+++ js/manzanero.js
@@ -0,0 +1,18 @@
+alert("MANZANO CHUCHERO");
+
+var name="Kevin"; 
+
+var edad=30;
+
+//alert(name);
+
+name=prompt("Dinos tu nombre");
+
+
+alert(name+" Bienvenido al EL MANZANO CHUCHERO");
+
+edad=prompt("Que edad tienes "+name+" ?")
+
+//document.write("Bienvenido al EL MANZANO CHUCHERO");
+//document.writw(name);
+//console.log(name);
